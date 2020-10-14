d94c422c73c3529e370c341a6b1d8e0ddb0d0cf0
Alena Shashko <L.Shashko@mail.ru>
2020-10-14 15:55:11 +0300

    create ReviewForm component

--- src/components/add-review-page/add-review-page.jsx
+++ src/components/add-review-page/add-review-page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReviewForm from '../review-form/review-form';
 
 const AddReviewPage = (props) => {
   const {cinemaName, ratings, movie} = props;
@@ -49,28 +50,7 @@ const AddReviewPage = (props) => {
       </div>
 
       <div className="add-review">
-        <form action="#" className="add-review__form">
-          <div className="rating">
-            <div className="rating__stars">
-              {ratings.map((rating) => {
-                return (
-                  <React.Fragment key={rating}>
-                    <input className="rating__input" id={`star-${rating}`} type="radio" name="rating" value={rating.toString()} />
-                    <label className="rating__label" htmlFor={`star-${rating}`}>{`Rating ${rating}`}</label>
-                  </React.Fragment>
-                );
-              })}
-            </div>
-          </div>
-
-          <div className="add-review__text">
-            <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
-            <div className="add-review__submit">
-              <button className="add-review__btn" type="submit">Post</button>
-            </div>
-
-          </div>
-        </form>
+        <ReviewForm ratings={ratings} />
       </div>
     </section>
   );
---
+++ src/components/review-form/review-form.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ReviewForm = (props) => {
+  const {ratings} = props;
+
+  return (
+    <form action="#" className="add-review__form">
+      <div className="rating">
+        <div className="rating__stars">
+          {ratings.map((rating) => {
+            return (
+              <React.Fragment key={rating}>
+                <input className="rating__input" id={`star-${rating}`} type="radio" name="rating" value={rating.toString()} />
+                <label className="rating__label" htmlFor={`star-${rating}`}>{`Rating ${rating}`}</label>
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="add-review__text">
+        <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+        <div className="add-review__submit">
+          <button className="add-review__btn" type="submit">Post</button>
+        </div>
+
+      </div>
+    </form>
+  );
+};
+
+ReviewForm.propTypes = {
+  ratings: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default ReviewForm;
