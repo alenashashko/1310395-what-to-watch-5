2ea537abd027bf18d2133282552baa636d31a962
Alena Shashko <L.Shashko@mail.ru>
2020-11-03 14:36:04 +0300

    create tabs component

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -6,6 +6,7 @@ import MoviesList from '../movies-list/movies-list';
 import MovieOverview from '../movie-overview/movie-overview';
 // import MovieDetails from '../movie-details/movie-details';
 // import MovieReviews from '../movie-reviews/movie-reviews';
+import Tabs from '../tabs/tabs';
 
 const MoviePage = (props) => {
   const {cinemaName, movie, movies} = props;
@@ -74,17 +75,7 @@ const MoviePage = (props) => {
 
             <div className="movie-card__desc">
               <nav className="movie-nav movie-card__nav">
-                <ul className="movie-nav__list">
-                  <li className="movie-nav__item movie-nav__item--active">
-                    <a href="#" className="movie-nav__link">Overview</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Details</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Reviews</a>
-                  </li>
-                </ul>
+                <Tabs />
               </nav>
 
               <MovieOverview movie={movie}/>
---
+++ src/components/tabs/tabs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Tabs = (props) => {
+  return (
+    <ul className="movie-nav__list">
+      <li className="movie-nav__item movie-nav__item--active">
+        <a href="#" className="movie-nav__link">Overview</a>
+      </li>
+      <li className="movie-nav__item">
+        <a href="#" className="movie-nav__link">Details</a>
+      </li>
+      <li className="movie-nav__item">
+        <a href="#" className="movie-nav__link">Reviews</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Tabs;
+
