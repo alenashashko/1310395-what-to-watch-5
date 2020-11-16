d0f685841750984914e69fc58b72230ef539150f
Alena Shashko <L.Shashko@mail.ru>
2020-11-16 03:20:44 +0300

    to plug redux to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 import App from './components/app/app';
 import {PROMO_MOVIE} from './mocks/movies'; // separate request
 import {MOVIES} from './mocks/movies';
 import {REVIEWS} from './mocks/reviews';
+import {reducer} from './store/reducer';
 
 const CINEMA_NAME = `WTW`;
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDom.render(
-    <App cinemaName={CINEMA_NAME} promoMovie={PROMO_MOVIE} movies={MOVIES} reviews={REVIEWS}/>,
+    <Provider store={store}>
+      <App cinemaName={CINEMA_NAME} promoMovie={PROMO_MOVIE} movies={MOVIES} reviews={REVIEWS}/>
+    </Provider>,
     document.querySelector(`#root`)
 );
