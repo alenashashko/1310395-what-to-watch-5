52eb051f8426da38b88e92600eab2d2041f69ddc
Alena Shashko <L.Shashko@mail.ru>
2020-11-18 03:25:37 +0300

    start to realize player logic

--- src/components/player-page/player-page.jsx
+++ src/components/player-page/player-page.jsx
@@ -1,47 +1,93 @@
-import React from 'react';
+import React, {createRef} from 'react';
 
 import proptypes from '../../type';
 import {formatMovieDuration} from '../../utils';
 
-const PlayerPage = (props) => {
-  const {movie} = props;
-  // id
+class PlayerPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="player">
-      <video src={movie.src} className="player__video" poster={movie.poster}></video>
+    this.state = {
+      isPlaying: false,
+    };
 
-      <button type="button" className="player__exit">Exit</button>
+    this._videoRef = createRef();
 
-      <div className="player__controls">
-        <div className="player__controls-row">
-          <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style={{left: `30%`}}>Toggler</div>
+    this._onFullscreenClick = this._onFullscreenClick.bind(this);
+    this._onPlayClick = this._onPlayClick.bind(this);
+    this._onPauseClick = this._onPauseClick.bind(this);
+  }
+
+  _onFullscreenClick() {
+    this._videoRef.current.requestFullscreen();
+  }
+
+  _onPlayClick() {
+    this._videoRef.current.play();
+
+    this.setState({
+      isPlaying: true,
+    });
+  }
+
+  _onPauseClick() {
+    this._videoRef.current.pause();
+
+    this.setState({
+      isPlaying: false,
+    });
+  }
+
+  render() {
+    const {movie} = this.props;
+    // id
+
+    return (
+      <div className="player">
+        <video ref={this._videoRef} src={movie.src} className="player__video" poster={movie.poster}></video>
+
+        <button type="button" className="player__exit">Exit</button>
+
+        <div className="player__controls">
+          <div className="player__controls-row">
+            <div className="player__time">
+              <progress className="player__progress" value="30" max="100"></progress>
+              <div className="player__toggler" style={{left: `30%`}}>Toggler</div>
+            </div>
+            <div className="player__time-value">{formatMovieDuration(movie.duration)}</div>
           </div>
-          <div className="player__time-value">{formatMovieDuration(movie.duration)}</div>
-        </div>
 
-        <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
-          <div className="player__name">Transpotting</div>
+          <div className="player__controls-row">
+            {this.state.isPlaying ? (
+              <button type="button" className="player__play" onClick={this._onPauseClick}>
+                <svg viewBox="0 0 14 21" width="14" height="21">
+                  <use xlinkHref="#pause"></use>
+                </svg>
+                <span>Pause</span>
+              </button>
+            ) : (
+              <button type="button" className="player__play" onClick={this._onPlayClick}>
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </button>
+            )}
 
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use xlinkHref="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
+            <div className="player__name">Transpotting</div>
+
+            <button type="button" className="player__full-screen" onClick={this._onFullscreenClick}>
+              <svg viewBox="0 0 27 27" width="27" height="27">
+                <use xlinkHref="#full-screen"></use>
+              </svg>
+              <span>Full screen</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 PlayerPage.propTypes = {
   id: proptypes.id,
