2a5001c59a489dc4d5cfdbf4109ff83ae5327c3d
Alena Shashko <L.Shashko@mail.ru>
2020-11-24 01:23:15 +0300

    get favoriteMovies from my-list-page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {REVIEWS} from './mocks/reviews';
 import rootReducer from './store/reducers/root-reducer';
 import {createAPI} from './services/api';
 import {changeAuthorizationStatus} from './store/action';
-import {fetchMoviesList, fetchFavoriteMoviesList, checkAuth} from './store/api-actions';
+import {fetchMoviesList, checkAuth} from './store/api-actions';
 
 const api = createAPI(() => store.dispatch(changeAuthorizationStatus(AuthorizationStatus.NO_AUTH)));
 
@@ -23,7 +23,6 @@ const store = createStore( // delete movies prop
 );
 
 store.dispatch(fetchMoviesList());
-store.dispatch(fetchFavoriteMoviesList()); // ?
 store.dispatch(checkAuth());
 
 ReactDom.render(
