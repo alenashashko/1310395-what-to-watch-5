11279f839226e65e79ecc1375c528a944e250bc9
Alena Shashko <L.Shashko@mail.ru>
2020-11-24 18:03:06 +0300

    refactor auth-page component

--- src/components/auth-page/auth-page.jsx
+++ src/components/auth-page/auth-page.jsx
@@ -1,67 +1,100 @@
-import React from 'react';
+import React, {PureComponent, createRef} from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import proptypes from '../../type';
+import {login} from '../../store/api-actions';
 
-const AuthPage = (props) => {
-  const {cinemaName} = props;
+class AuthPage extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="user-page">
-      <header className="page-header user-page__head">
-        <div className="logo">
-          <Link to="/" className="logo__link">
-            {cinemaName.split(``).map((character, index) => {
-              return (
-                <span key={index} className={`logo__letter logo__letter--${index + 1}`}>{character}</span>
-              );
-            })}
-          </Link>
+    this._loginRef = createRef();
+    this._passwordRef = createRef();
+
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleSubmit(evt) {
+    const {onSubmit} = this.props;
+
+    evt.preventDefault();
+
+    onSubmit({
+      email: this._loginRef.current.value,
+      password: this._passwordRef.current.value,
+    });
+  }
+
+  render() {
+    const {cinemaName} = this.props;
+
+    return (
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <div className="logo">
+            <Link to="/" className="logo__link">
+              {cinemaName.split(``).map((character, index) => {
+                return (
+                  <span key={index} className={`logo__letter logo__letter--${index + 1}`}>{character}</span>
+                );
+              })}
+            </Link>
+          </div>
+
+          <h1 className="page-title user-page__title">Sign in</h1>
+        </header>
+
+        <div className="sign-in user-page__content">
+          <form onSubmit={this._handleSubmit} action="#" className="sign-in__form">
+            <div className="sign-in__fields">
+              <div className="sign-in__field">
+                <input ref={this._loginRef} className="sign-in__input" type="email"
+                  placeholder="Email address" name="user-email" id="user-email" />
+                <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+              </div>
+              <div className="sign-in__field">
+                <input ref={this._passwordRef} className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+                <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+              </div>
+            </div>
+            <div className="sign-in__submit">
+              <button className="sign-in__btn" type="submit">Sign in</button>
+            </div>
+          </form>
         </div>
 
-        <h1 className="page-title user-page__title">Sign in</h1>
-      </header>
+        <footer className="page-footer">
+          <div className="logo">
+            <Link to="/" className="logo__link logo__link--light">
+              {cinemaName.split(``).map((character, index) => {
+                return (
+                  <span key={index} className={`logo__letter logo__letter--${index + 1}`}>{character}</span>
+                );
+              })}
+            </Link>
+          </div>
 
-      <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
-          <div className="sign-in__fields">
-            <div className="sign-in__field">
-              <input className="sign-in__input" type="email"
-                placeholder="Email address" name="user-email" id="user-email" />
-              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
-            </div>
-            <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
-              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
-            </div>
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
           </div>
-          <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
-          </div>
-        </form>
+        </footer>
       </div>
-
-      <footer className="page-footer">
-        <div className="logo">
-          <Link to="/" className="logo__link logo__link--light">
-            {cinemaName.split(``).map((character, index) => {
-              return (
-                <span key={index} className={`logo__letter logo__letter--${index + 1}`}>{character}</span>
-              );
-            })}
-          </Link>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
-    </div>
-  );
-};
+    );
+  }
+}
 
 AuthPage.propTypes = {
-  cinemaName: proptypes.cinemaName
+  cinemaName: proptypes.cinemaName,
+  onSubmit: PropTypes.func.isRequired
 };
 
-export default AuthPage;
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  }
+});
+
+export {AuthPage};
+export default connect(null, mapDispatchToProps)(AuthPage);
--- src/store/reducers/root-reducer.js
+++ src/store/reducers/root-reducer.js
@@ -1,13 +1,16 @@
 import {combineReducers} from 'redux';
 import {appFunctionality} from '../reducers/app-functionality/app-functionality';
 import {appData} from '../reducers/app-data/app-data';
+import {user} from './user/user';
 
 export const NameSpace = {
   DATA: `DATA`,
-  APP: `APP`
+  APP: `APP`,
+  USER: `USER`
 };
 
 export default combineReducers({
   [NameSpace.DATA]: appData,
-  [NameSpace.APP]: appFunctionality
+  [NameSpace.APP]: appFunctionality,
+  [NameSpace.USER]: user
 });
